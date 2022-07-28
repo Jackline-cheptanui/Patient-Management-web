f6e2af780952fc728ad34156b8e9f2efd6a0eb73
caro-mugo <mugonycaroline@gmail.com>
2022-07-28 21:45:25 +0300

    models commit

--- api/serializers.py
+++ api/serializers.py
@@ -1,22 +1,22 @@
-# from rest_framework import serializers
-# from patient.models import Patient
-# from vital.models import Vital
-# from visit.models import Visit
+from rest_framework import serializers
+from patient.models import Patient
+from vital.models import Vital
+from visit.models import Visit
 
 
-# class PatientSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model=Patient
-#         fields=('first_name','last_name','age')
+class PatientSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Patient
+        fields=('patient_number','last_name','first_name')
 
-# class VisitSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model=Visit
-#         fields=('first_name','last_name','bio')
+class VitalSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Visit
+        fields=(' patient_name','patient_visit_date')
 
 
-# class VitalSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model=Vital
-#         fields=('event_name','venue','description')
+class VisitSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Visit
+        fields=('comments',' general_health')
 
--- api/urls.py
+++ api/urls.py
@@ -1,12 +1,12 @@
-# from django.urls import path,include
-# from rest_framework import routers
-# from .views import *
+from django.urls import path,include
+from rest_framework import routers
+from .views import *
 
-# router=routers.DefaultRouter()
-# router.register("patients",PatientViewSet)
-# router.register("visits",VisitViewSet)
-# router.register("vitals",VitalViewSet)
+router=routers.DefaultRouter()
+router.register("patients",PatientViewSet)
+router.register("visits",VisitViewSet)
+router.register("vitals",VitalViewSet)
 
-# urlpatterns=[
-#     path("",include(router.urls))
-# ]
+urlpatterns=[
+    path("",include(router.urls))
+]
